608a1178b6d27fb037f6fac650f9ffe95458c2c8
vijay-kumar-8 <vijisnv97@gmail.com>
2025-10-19 14:03:23 +0530

    Changes related to module structure

---
+++ src/app/admin/modules/admin-home/admin-home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminHomeRoutingModule } from './admin-home-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminHomeRoutingModule
+  ]
+})
+export class AdminHomeModule { }
---
+++ src/app/provider/modules/provider-home/provider-home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProviderHomeRoutingModule } from './provider-home-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProviderHomeRoutingModule
+  ]
+})
+export class ProviderHomeModule { }
